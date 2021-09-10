e32a8b14beb2c424375ac4a12e8c31eb97237e9f
Nicolas Grobelny <miki4920@gmail.com>
2021-09-10 21:30:08 +0100

    Added client side sockets

--- static/connect_four.js
+++ static/connect_four.js
@@ -1,4 +1,14 @@
 function send_command(command, argument="") {
-    document.getElementById("command").value = command + ":" + argument;
-    document.getElementById("command_form").submit();
+    let message = command + ":" + argument;
+    socket.emit("connect_four_update", message);
 }
+
+
+let socket = io.connect("http://127.0.0.1:5000/")
+
+socket.on('connect_four_board', (msg) => {
+    document.querySelector('html').innerHTML = msg;
+});
+
+
+
