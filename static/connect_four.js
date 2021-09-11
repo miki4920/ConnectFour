de393e007d1ca6caeadbf42b20c29f49b9d708eb
Nicolas Grobelny <miki4920@gmail.com>
2021-09-11 13:04:11 +0100

    Implement sockets for receiving all data (Board, player, winner)

--- static/connect_four.js
+++ static/connect_four.js
@@ -3,12 +3,33 @@ function send_command(command, argument="") {
     socket.emit("connect_four_update", message);
 }
 
-
 let socket = io.connect("http://127.0.0.1:5000/")
+let cookie = document.cookie
+if(!cookie) {
+    document.cookie = "userid=" + Math.floor(Math.random()*100000)
+}
+let id = document.cookie.split("=")[1]
 
-socket.on('connect_four_board', (msg) => {
-    document.querySelector('html').innerHTML = msg;
+socket.on('connect_four_board' + id, (message) => {
+    for (let [key, value] of Object.entries(message)) {
+        let element = document.getElementById(key);
+        element.classList.remove(...element.classList);
+        if(value) {
+            element.classList.add("connect_four_element_" + value)
+        }
+        else {
+            element.classList.add("connect_four_element")
+        }
+    }
 });
+
+socket.on('connect_four_player' + id, (message) => {
+    document.getElementById("player").innerHTML = "Player: " + message;
+})
+
+socket.on('connect_four_winner' + id, (message) => {
+    document.getElementById("winner").innerHTML = "Winner: " + message;
+})
 
 
 
