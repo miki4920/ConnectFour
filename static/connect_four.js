5d70b9f794b860cbd431430156f4758339510736
Nicolas Grobelny <miki4920@gmail.com>
2021-09-12 14:36:23 +0100

    Implemented a back button

--- static/connect_four.js
+++ static/connect_four.js
@@ -1,3 +1,8 @@
+function back()  {
+    window.location.href = "http://127.0.0.1:5000/";
+}
+
+
 function send_command(command, argument="") {
     let message = command + ":" + argument;
     socket.emit("connect_four_update", message);
