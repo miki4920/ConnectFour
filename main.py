2a1e7b05995863963af4eaff48e9ddaf9ffc39cf
Nicolas Grobelny <miki4920@gmail.com>
2021-09-12 22:48:35 +0100

    Added handling based on usernames stored on the server rather than cookies

--- main.py
+++ main.py
@@ -1,47 +1,43 @@
 import json
+import os
 
 from flask import Flask, make_response, render_template, request, redirect
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, ConnectionRefusedError
 
 from board import ConnectFour, generate_board
 from config import Config
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+current_players = []
 
 
-def get_user_data(request):
-    try:
-        with open(f"games/{request.cookies.get('userid')}.json", "r") as file:
+def get_user_data(username=None):
+    if username and os.path.exists(f"games/{username}.json"):
+        with open(f"games/{username}.json", "r") as file:
             return json.load(file)
-    except FileNotFoundError:
-        return {"board": generate_board(), "player": True}
+    return {"board": generate_board(), "player": True}
 
 
-def set_user_data(request, board, player):
+def set_user_data(username, board, player):
     data_dictionary = {"board": board, "player": player}
-    with open(f"games/{request.cookies.get('userid')}.json", "w") as file:
+    with open(f"games/{username}.json", "w") as file:
         json.dump(data_dictionary, file)
 
 
-@app.route("/", methods=["GET", "POST"])
-def entry_screen():
-    if request.method == "POST":
-        if not request.form.get("mode"):
-            return redirect("/single-player")
-        return redirect("/multi-player")
-    response = make_response(render_template('entry_page.html', board=ConnectFour().board, player=Config.player_one_name))
-    return response
-
-
-@app.route("/single-player", methods=["GET"])
 def single_player():
-    user_data = get_user_data(request)
+    user_data = get_user_data()
     connect_four = ConnectFour(user_data["board"])
     player = user_data["player"]
     winner = connect_four.check_winner()
+    return connect_four.board, player, winner
+
+
+@app.route("/", methods=["GET"])
+def main():
+    board, player, winner = single_player()
     response = make_response(
-        render_template('connect_four.html', board=connect_four.board, player=player, player_one=Config.player_one,
+        render_template('connect_four.html', board=board, player=player, player_one=Config.player_one,
                         player_one_name=Config.player_one_name,
                         player_two=Config.player_two, player_two_name=Config.player_two_name,
                         width=Config.width, winner=winner))
@@ -70,23 +66,54 @@ def reset_board(request, argument):
     return connect_four, player, winner
 
 
-@socketio.on("connect_four_update")
-def connect_four_post_socket(message):
-    command_dictionary = {"add": add_element,
-                          "reset": reset_board}
-    command = message.split(":")
-    command, argument = command[0], command[1:]
-    connect_four, player, winner = command_dictionary[command](request, argument)
-    user_data = get_user_data(request)
-    player_board = ConnectFour(user_data["board"])
-    set_user_data(request, connect_four.board, player)
-    user_id = request.cookies.get("userid")
-    emit("connect_four_board"+user_id, connect_four-player_board)
-    emit("connect_four_player"+user_id, Config.player_one_name if player else Config.player_two_name)
-    if not winner:
-        winner = "None"
-    emit("connect_four_winner"+user_id, winner)
+@socketio.on("connect", namespace="/")
+def on_connect(auth):
+    username = auth.get('username')
+    if username:
+        current_players.append(username)
+        emit("players", len(current_players))
+    else:
+        raise ConnectionRefusedError('You must provide username!')
+
+    @socketio.on("disconnect")
+    def on_disconnect():
+        current_players.remove(username)
+
+    @socketio.on("singleplayer")
+    def single_player_board():
+        user_data = get_user_data(username)
+        board = ConnectFour(user_data["board"])
+        player = user_data["player"]
+        winner = board.check_winner()
+        emit("connect_four_board" + username, board - ConnectFour())
+        emit("connect_four_player" + username, Config.player_one_name if player else Config.player_two_name)
+        if not winner:
+            winner = "None"
+        emit("connect_four_winner" + username, winner)
+
+    @socketio.on("connect_four_update")
+    def single_player_update(message):
+        command_dictionary = {"add": add_element,
+                              "reset": reset_board}
+        command = message.split(":")
+        command, argument = command[0], command[1:]
+        connect_four, player, winner = command_dictionary[command](username, argument)
+        user_data = get_user_data(username)
+        player_board = ConnectFour(user_data["board"])
+        set_user_data(username, connect_four.board, player)
+        emit("connect_four_board"+username, connect_four-player_board)
+        emit("connect_four_player"+username, Config.player_one_name if player else Config.player_two_name)
+        if not winner:
+            winner = "None"
+        emit("connect_four_winner"+username, winner)
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
     socketio.run(app)
+
