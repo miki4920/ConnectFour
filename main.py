b840f69000c1e370a352222483fefcbc5cb3609a
Nicolas Grobelny <miki4920@gmail.com>
2021-09-14 18:10:13 +0100

    Fixed an error when two connected users couldn't play simulteniously. Added a class for user

--- main.py
+++ main.py
@@ -8,34 +8,37 @@ from board import ConnectFour, generate_board
 from config import Config
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
 socketio = SocketIO(app)
-current_players = []
+current_players = {}
 
 
-def get_user_data(username=None):
-    if username and os.path.exists(f"games/{username}.json"):
-        with open(f"games/{username}.json", "r") as file:
-            return json.load(file)
-    return {"board": generate_board(), "player": True}
+class User:
+    def __init__(self, session_id, username):
+        self.session_id = session_id
+        self.username = username
 
+    def __eq__(self, other):
+        return self.username == other
 
-def set_user_data(username, board, player):
-    data_dictionary = {"board": board, "player": player}
-    with open(f"games/{username}.json", "w") as file:
-        json.dump(data_dictionary, file)
+    def get_user_data(self):
+        if self.username and os.path.exists(f"games/{self.username}.json"):
+            with open(f"games/{self.username}.json", "r") as file:
+                data_file = json.load(file)
+                return ConnectFour(data_file["board"]), data_file["player"]
+        return ConnectFour(generate_board()), True
 
-
-def single_player():
-    user_data = get_user_data()
-    connect_four = ConnectFour(user_data["board"])
-    player = user_data["player"]
-    winner = connect_four.check_winner()
-    return connect_four.board, player, winner
+    def set_user_data(self, board, player):
+        data_dictionary = {"board": board, "player": player}
+        with open(f"games/{self.username}.json", "w") as file:
+            json.dump(data_dictionary, file)
 
 
 @app.route("/", methods=["GET"])
 def main():
-    board, player, winner = single_player()
+    board = generate_board()
+    player = Config.player_one_name
+    winner = ""
     response = make_response(
         render_template('connect_four.html', board=board, player=player, player_one=Config.player_one,
                         player_one_name=Config.player_one_name,
@@ -44,11 +47,8 @@ def main():
     return response
 
 
-def add_element(request, argument):
-    assert len(argument) == 1
-    user_data = get_user_data(request)
-    connect_four = ConnectFour(user_data["board"])
-    player = user_data["player"]
+def add_element(user, argument):
+    connect_four, player = user.get_user_data()
     winner = connect_four.check_winner()
     if not winner:
         if connect_four.add_element(int(argument[0]), player):
@@ -57,63 +57,57 @@ def add_element(request, argument):
     return connect_four, player, winner
 
 
-def reset_board(request, argument):
-    connect_four = ConnectFour(request)
+def reset_board(user, argument):
+    connect_four, player = user.get_user_data()
     connect_four.reset_board()
-    user_data = get_user_data(request)
-    player = user_data["player"]
-    winner = None
+    winner = ""
     return connect_four, player, winner
 
 
-@socketio.on("connect", namespace="/")
+@socketio.on("connect")
 def on_connect(auth):
-    username = auth.get('username')
-    if username:
-        current_players.append(username)
-        emit("players", len(current_players))
+    username = auth.get("username")
+    if username and username not in current_players:
+        current_players[request.sid] = (User(request.sid, username))
+        emit("players", len(current_players), broadcast=True)
     else:
-        raise ConnectionRefusedError('You must provide username!')
-
-    @socketio.on("disconnect")
-    def on_disconnect():
-        current_players.remove(username)
-
-    @socketio.on("singleplayer")
-    def single_player_board():
-        user_data = get_user_data(username)
-        board = ConnectFour(user_data["board"])
-        player = user_data["player"]
-        winner = board.check_winner()
-        emit("connect_four_board" + username, board - ConnectFour())
-        emit("connect_four_player" + username, Config.player_one_name if player else Config.player_two_name)
-        if not winner:
-            winner = "None"
-        emit("connect_four_winner" + username, winner)
-
-    @socketio.on("connect_four_update")
-    def single_player_update(message):
-        command_dictionary = {"add": add_element,
-                              "reset": reset_board}
-        command = message.split(":")
-        command, argument = command[0], command[1:]
-        connect_four, player, winner = command_dictionary[command](username, argument)
-        user_data = get_user_data(username)
-        player_board = ConnectFour(user_data["board"])
-        set_user_data(username, connect_four.board, player)
-        emit("connect_four_board"+username, connect_four-player_board)
-        emit("connect_four_player"+username, Config.player_one_name if player else Config.player_two_name)
-        if not winner:
-            winner = "None"
-        emit("connect_four_winner"+username, winner)
+        raise ConnectionRefusedError('You must provide a unique username!')
 
 
+@socketio.on("disconnect")
+def on_disconnect():
+    del current_players[request.sid]
 
 
+@socketio.on("singleplayer")
+def single_player_board():
+    user = current_players[request.sid]
+    connect_four, player = user.get_user_data()
+    player = Config.player_one_name if player else Config.player_two_name
+    winner = connect_four.check_winner()
+    winner = winner if winner else "None"
+    emit("connect_four_board", {"connect_four": connect_four - ConnectFour(), "player": player, "winner": winner},
+         room=user.session_id)
 
 
+@socketio.on("connect_four_update")
+def single_player_update(message):
+    command_dictionary = {"add": add_element,
+                          "reset": reset_board}
+    command = message.split(":")
+    command, argument = command[0], command[1:]
+    user = current_players[request.sid]
+    player_board, player = user.get_user_data()
+
+    connect_four, player, winner = command_dictionary[command](user, argument)
+    user.set_user_data(connect_four.board, player)
+
+    connect_four = connect_four - player_board
+    player = Config.player_one_name if player else Config.player_two_name
+    winner = winner if winner else "None"
+
+    emit("connect_four_board", {"connect_four": connect_four, "player": player, "winner": winner}, room=user.session_id)
 
 
 if __name__ == '__main__':
     socketio.run(app)
-
